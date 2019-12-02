4da6ae78365d03b44020ec211442baf92f298a76
Ege Melis <eayanoglu15@ku.edu.tr>
2019-12-02 19:41:20 +0300

    Removed base viewController

---
+++ iOS_Final_Project/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  iOS_Final_Project
+//
+//  Created by Ege Melis Ayanoğlu on 2.12.2019.
+//  Copyright © 2019 Bogo. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
