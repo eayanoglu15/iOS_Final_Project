0aaddec5bf063a11afc02733e94f8d0a04c26dcf
Ege Melis <eayanoglu15@ku.edu.tr>
2019-12-03 23:25:01 +0300

    Added New Hitchhiker Account Screen and New Driver Account Screen

---
+++ iOS_Final_Project/ViewController/NewHitchhikerAccountViewController.swift
@@ -0,0 +1,89 @@
+//
+//  NewHitchhikerAccountViewController.swift
+//  iOS_Final_Project
+//
+//  Created by Ege Melis Ayanoğlu on 3.12.2019.
+//  Copyright © 2019 Bogo. All rights reserved.
+//
+
+import UIKit
+
+class NewHitchhikerAccountViewController: BaseScrollViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate  {
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var surnameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var phoneNumberTextField: UITextField!
+    @IBOutlet weak var ageTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Create Hitchhiker Account"
+        
+        // Choose Picture
+        profileImageView.isUserInteractionEnabled = true
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(choosePicture))
+        profileImageView.addGestureRecognizer(gestureRecognizer)
+        
+        // Assign Text Field Delegates
+        usernameTextField.delegate = self
+        passwordTextField.delegate = self
+        nameTextField.delegate = self
+        surnameTextField.delegate = self
+        emailTextField.delegate = self
+        phoneNumberTextField.delegate = self
+        ageTextField.delegate = self
+    }
+    
+    @objc func choosePicture() {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        self.present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        profileImageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func registerButtonTapped(_ sender: Any) {
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+//MARK: - UITextFieldDelegate
+
+extension NewHitchhikerAccountViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+        // self.view.endEditing(true)
+        // return false
+    }
+    
+    @objc
+    func textFieldDidChange(textField: UITextField) {
+        /*
+        if model.isUsernameValid(username: username.text ?? "") && model.isPasswordValid(password: password.text ?? "") {
+            loginButton.isEnabled = true
+        } else {
+            loginButton.isEnabled = false
+        }
+        */
+    }
+}
